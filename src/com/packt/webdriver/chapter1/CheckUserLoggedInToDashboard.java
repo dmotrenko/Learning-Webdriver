91d92df6bb7ad2eb7eef70497a73c6018570d5ce
Dmitriy Motrenko <dmotren@microsoft.com>
2014-09-09 21:59:45 +0400

    add page object, page factory, FindBy, checkTitle

--- src/com/packt/webdriver/chapter1/CheckUserLoggedInToDashboard.java
+++ src/com/packt/webdriver/chapter1/CheckUserLoggedInToDashboard.java
@@ -1,38 +1,26 @@
 package com.packt.webdriver.chapter1;
-import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import wordpress.testselenium2014.tests.pageobjects.LoginPage;
 
 
 
-public class NavigateToAUrl {
+public class CheckUserLoggedInToDashboard {
 	public static void main(String[] args){
-		
-		
 		WebDriver driver = new FirefoxDriver();
-		
-		//login to site
-		LoginPage lgnPage = new LoginPage(driver);
+		LoginPage lgnPage = PageFactory.initElements(driver, LoginPage.class);
                 lgnPage.login("testselenium2014", "velosiped");
-		
-		//verify that we see dashboad
-		WebElement dashboardTitle = driver.findElement(By.cssSelector("#wpbody-content > div.wrap > h2"));
-		String dashboardTitleText = dashboardTitle.getText();
-		assertEquals("Dashboard",dashboardTitleText);
-                
+		lgnPage.checkTitle("Dashboard");
                 //explicit wait
                 //verify that Dashboard title contains string "dashboard"
                 WebDriverWait wait = new WebDriverWait(driver, 10);
                 wait.until(ExpectedConditions.titleContains("Dashboard"));
-                
-                
 		driver.close();}
 		
    }    
--- src/wordpress/testselenium2014/tests/pageobjects/LoginPage.java
+++ src/wordpress/testselenium2014/tests/pageobjects/LoginPage.java
@@ -1,30 +1,33 @@
 package wordpress.testselenium2014.tests.pageobjects;
+import static org.junit.Assert.assertEquals;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.*;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage { 
     
     WebDriver driver;
-    WebElement login;
-    WebElement password;
-    WebElement submit;
+    @FindBy(how=How.ID, id="user_login") WebElement login;
+    @FindBy(how=How.ID, id="user_pass") WebElement password;
+    @FindBy(how=How.ID, id="wp-submit") WebElement submit;
+    @FindBy(css = "#wpbody-content > div.wrap > h2") WebElement dashboardTitle;
+    String url = "https://testselenium2014.wordpress.com/wp-login.php";
     
-    
-    //class cunstructor
     public LoginPage(WebDriver driver){
         this.driver = driver;
-        driver.get("https://testselenium2014.wordpress.com/wp-login.php");
-        login = driver.findElement(By.id("user_login"));
-        password = driver.findElement(By.id("user_pass"));
-        submit = driver.findElement(By.id("wp-submit"));
-        
+        driver.get(url); 
     }
     
     public void login(String username,String userPassword ){
         login.sendKeys(username);
         password.sendKeys(userPassword);
         submit.click();
+    }
+    
+    public void checkTitle(String title) {
+       String dashboardTitleText = dashboardTitle.getText();
+       assertEquals(title,dashboardTitleText);
     }
 };
